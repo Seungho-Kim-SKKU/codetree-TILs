9b006c22fcb06d5f080152e3cf2158722eda3557
Seungho Kim <kimsho98@naver.com>
2024-10-07 21:42:19 +0900

    Title: 고대 문명 유적 탐사, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241007/고대 문명 유적 탐사/ancient-ruin-exploration.cpp
+++ 241007/고대 문명 유적 탐사/ancient-ruin-exploration.cpp
@@ -1,37 +1,23 @@
 #include <iostream>
-#include <unordered_map>
+#include <algorithm>
 #include <queue>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-#define endl "\n"
-
 int K, M;
-int sumScore;
-int umul[7][7];
-int curUmul[7][7];
-int bestUmul[7][7];
-bool visit[7][7] = { 0, };
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-queue<int> nextUmul;
+int map[7][7];
+int curMap[7][7];
+int bestMap[7][7];
+bool visit[7][7];
+int di[4] = {1, 0, -1, 0};
+int dj[4] = {0, 1, 0, -1};
+queue<int> qUmul;
 
-void printUmul() {
+void printArray(int a[][7]) {
     for (int i = 1; i < 6; i++) {
         for (int j = 1; j < 6; j++) {
-            cout << umul[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-
-void printCurUmul() {
-    for (int i = 1; i < 6; i++) {
-        for (int j = 1; j < 6; j++) {
-            cout << curUmul[i][j] << " ";
+            cout << a[i][j] << " ";
         }
         cout << endl;
     }
@@ -49,67 +35,48 @@ void initVisit() {
             }
         }
     }
-
-    return;
 }
 
-void rotate90(int a, int b) {
-    for (int i = 1; i < 6; i++) {
-        for (int j = 1; j < 6; j++) {
-            curUmul[i][j] = umul[i][j];
-        }
+void rotateMap(int i, int j, int d) {
+    copy(&map[0][0], &map[0][0] + 7 * 7, &curMap[0][0]);
+
+    if (d == 0) {
+        curMap[i-1][j-1] = map[i+1][j-1];
+        curMap[i-1][j] = map[i][j-1];
+        curMap[i-1][j+1] = map[i-1][j-1];
+        curMap[i][j-1] = map[i+1][j];
+        curMap[i][j+1] = map[i-1][j];
+        curMap[i+1][j-1] = map[i+1][j+1];
+        curMap[i+1][j] = map[i][j+1];
+        curMap[i+1][j+1] = map[i-1][j+1];
     }
-    curUmul[a-1][b-1] = umul[a+1][b-1];
-    curUmul[a-1][b] = umul[a][b-1];
-    curUmul[a-1][b+1] = umul[a-1][b-1];
-    curUmul[a][b-1] = umul[a+1][b];
-    curUmul[a][b+1] = umul[a-1][b];
-    curUmul[a+1][b-1] = umul[a+1][b+1];
-    curUmul[a+1][b] = umul[a][b+1];
-    curUmul[a+1][b+1] = umul[a-1][b+1];
-
-    return;
-}
-
-void rotate180(int a, int b) {
-    for (int i = 1; i < 6; i++) {
-        for (int j = 1; j < 6; j++) {
-            curUmul[i][j] = umul[i][j];
-        }
+    else if (d == 1) {
+        curMap[i-1][j-1] = map[i+1][j+1];
+        curMap[i-1][j] = map[i+1][j];
+        curMap[i-1][j+1] = map[i+1][j-1];
+        curMap[i][j-1] = map[i][j+1];
+        curMap[i][j+1] = map[i][j-1];
+        curMap[i+1][j-1] = map[i-1][j+1];
+        curMap[i+1][j] = map[i-1][j];
+        curMap[i+1][j+1] = map[i-1][j-1];
     }
-    curUmul[a-1][b-1] = umul[a+1][b+1];
-    curUmul[a-1][b] = umul[a+1][b];
-    curUmul[a-1][b+1] = umul[a+1][b-1];
-    curUmul[a][b-1] = umul[a][b+1];
-    curUmul[a][b+1] = umul[a][b-1];
-    curUmul[a+1][b-1] = umul[a-1][b+1];
-    curUmul[a+1][b] = umul[a-1][b];
-    curUmul[a+1][b+1] = umul[a-1][b-1];
-
-    return;
-}
-
-void rotate270(int a, int b) {
-    for (int i = 1; i < 6; i++) {
-        for (int j = 1; j < 6; j++) {
-            curUmul[i][j] = umul[i][j];
-        }
+    else {
+        curMap[i-1][j-1] = map[i-1][j+1];
+        curMap[i-1][j] = map[i][j+1];
+        curMap[i-1][j+1] = map[i+1][j+1];
+        curMap[i][j-1] = map[i-1][j];
+        curMap[i][j+1] = map[i+1][j];
+        curMap[i+1][j-1] = map[i-1][j-1];
+        curMap[i+1][j] = map[i][j-1];
+        curMap[i+1][j+1] = map[i+1][j-1];
     }
-    curUmul[a-1][b-1] = umul[a-1][b+1];
-    curUmul[a-1][b] = umul[a][b+1];
-    curUmul[a-1][b+1] = umul[a+1][b+1];
-    curUmul[a][b-1] = umul[a-1][b];
-    curUmul[a][b+1] = umul[a+1][b];
-    curUmul[a+1][b-1] = umul[a-1][b-1];
-    curUmul[a+1][b] = umul[a][b-1];
-    curUmul[a+1][b+1] = umul[a+1][b-1];
 
     return;
 }
 
 int calcScore() {
-    initVisit();
     int score = 0;
+    initVisit();
 
     for (int i = 1; i < 6; i++) {
         for (int j = 1; j < 6; j++) {
@@ -117,20 +84,20 @@ int calcScore() {
                 queue<pair<int, int>> q, trace;
                 q.push(make_pair(i, j));
                 trace.push(make_pair(i, j));
-                visit[i][j] = true;
 
-                while (!q.empty()) {
+                while(!q.empty()) {
+                    visit[i][j] = true;
                     pair<int, int> cur = q.front();
                     q.pop();
 
                     for (int k = 0; k < 4; k++) {
-                        int ny = cur.first + dy[k];
-                        int nx = cur.second + dx[k];
+                        int ni = cur.first + di[k];
+                        int nj = cur.second + dj[k];
 
-                        if (!visit[ny][nx] && curUmul[cur.first][cur.second] == curUmul[ny][nx]) {
-                            q.push(make_pair(ny, nx));
-                            trace.push(make_pair(ny, nx));
-                            visit[ny][nx] = true;
+                        if (!visit[ni][nj] && curMap[cur.first][cur.second] == curMap[ni][nj]) {
+                            q.push(make_pair(ni, nj));
+                            trace.push(make_pair(ni, nj));
+                            visit[ni][nj] = true;
                         }
                     }
                 }
@@ -140,33 +107,12 @@ int calcScore() {
                         score++;
                         pair<int, int> cur = trace.front();
                         trace.pop();
-                        curUmul[cur.first][cur.second] = 0;
+                        curMap[cur.first][cur.second] = 0;
                     }
                 }
             }
         }
     }
-    
-    return score;
-}
-
-int checkUmul90(int a, int b) {    
-    rotate90(a, b);
-    int score = calcScore();
-
-    return score;
-}
-
-int checkUmul180(int a, int b) {
-    rotate180(a, b);
-    int score = calcScore();
-
-    return score;
-}
-
-int checkUmul270(int a, int b) {
-    rotate270(a, b);
-    int score = calcScore();
 
     return score;
 }
@@ -175,79 +121,79 @@ int main() {
     cin >> K >> M;
 
     for (int i = 1; i < 6; i++) {
-        int n;
         for (int j = 1; j < 6; j++) {
-            cin >> n;
-            umul[i][j] = n;
+            int a;
+            cin >> a;
+            map[i][j] = a;
         }
     }
 
-    // printUmul();
+    // printArray(map);
 
     for (int i = 0; i < M; i++) {
-        int n;
-        cin >> n;
-        nextUmul.push(n);
+        int a;
+        cin >> a;
+        qUmul.push(a);
     }
 
     for (int k = 0; k < K; k++) {
-        sumScore = 0;
+        int sum = 0;
         int bestScore = 0;
-        for (int l = 0; l < 3; l++) {
-            for (int i = 2; i < 5; i++) {
-                for (int j = 2; j < 5; j++) {
-                    if (l == 0) {
-                        int score = checkUmul90(i, j);
-                        if (score > bestScore) {
-                            bestScore = score;
-                            copy(&curUmul[0][0], &curUmul[0][0] + 49, &bestUmul[0][0]);
-                        }
-                    }
-                    else if (l == 1) {
-                        int score = checkUmul180(i, j);
-                        if (score > bestScore) {
-                            bestScore = score;
-                            copy(&curUmul[0][0], &curUmul[0][0] + 49, &bestUmul[0][0]);
-                        }
-                    }
-                    else {
-                        int score = checkUmul270(i, j);
-                        if (score > bestScore) {
-                            bestScore = score;
-                            copy(&curUmul[0][0], &curUmul[0][0] + 49, &bestUmul[0][0]);
-                        }
+        // pair<int, int> bestIndex;
+        for (int d = 0; d < 3; d++) {
+            for (int j = 2; j < 5; j++) {
+                for (int i = 2; i < 5; i++) {
+                    // if (i == 3 && j ==3) printArray(curMap);
+
+                    int score = 0;
+
+                    rotateMap(i, j, d);
+
+                    score = calcScore();
+
+                    // cout << i << " " << j << " " << score << endl;
+
+                    if (score > bestScore) {
+                        bestScore = score;
+                        copy(&curMap[0][0], &curMap[0][0] + 7 * 7, &bestMap[0][0]);
+                        // printArray(bestMap);
+                        // bestIndex = make_pair(i, j);
                     }
                 }
             }
-        }        
-
+        }
         if (bestScore == 0) break;
+        // cout << bestIndex.first << bestIndex.second << endl;
+        sum += bestScore;
 
-        sumScore += bestScore;        
-
-        while (true) {
-            copy(&bestUmul[0][0], &bestUmul[0][0] + 49, &umul[0][0]);
+        while (1) {
+            copy(&bestMap[0][0], &bestMap[0][0] + 7 * 7, &map[0][0]);
 
             for (int j = 1; j < 6; j++) {
                 for (int i = 5; i > 0; i--) {
-                    if (umul[i][j] == 0) {
-                        int cur = nextUmul.front();
-                        nextUmul.pop();
-                        umul[i][j] = cur;
-                        bestScore--;
+                    if (map[i][j] == 0) {
+                        int cur = qUmul.front();
+                        map[i][j] = cur;
+                        qUmul.pop();
                     }
                 }
             }
+            // printArray(map);
 
-            copy(&umul[0][0], &umul[0][0] + 49, &curUmul[0][0]);
+            copy(&map[0][0], &map[0][0] + 7 * 7, &curMap[0][0]);
 
-            bestScore = calcScore();
-            if (bestScore == 0) break;
-            sumScore += bestScore;
+            int score = calcScore();
+            
+            if (score == 0) {
+                break;
+            }
 
-            copy(&curUmul[0][0], &curUmul[0][0] + 49, &bestUmul[0][0]);
+            sum += score;
+            // cout << score << endl;
+            // printArray(curMap);
+            copy(&curMap[0][0], &curMap[0][0] + 7 * 7, &bestMap[0][0]);
         }
-        cout << sumScore << " ";
+        cout << sum << " ";
     }
 
     return 0;
